b6ef112ec5808c28c6e52777db09bcc6c11380d4
Nawin chander <nawinnapzy289@gmail.com>
2026-03-28 11:18:42 +0530

    add code

---
+++ Advance/Least Connections.js
@@ -0,0 +1,39 @@
+class LeastConnectionBalancer {
+  constructor(servers) {
+    this.servers = servers.map((url) => ({
+      url,
+      activeConnections: 0,
+    }));
+  }
+
+  getServer() {
+    let least = this.servers[0];
+
+    for (const server of this.servers) {
+      if (server.activeConnections < least.activeConnections) {
+        least = server;
+      }
+    }
+
+    least.activeConnections++;
+    return least;
+  }
+
+  release(serverUrl) {
+    const server = this.servers.find(s => s.url === serverUrl);
+    if (server) server.activeConnections--;
+  }
+}
+
+// Usage
+const lb = new LeastConnectionBalancer([
+  "http://s1",
+  "http://s2",
+]);
+
+const server = lb.getServer();
+console.log(server.url);
+
+// after request finishes
+lb.release(server.url);
+
