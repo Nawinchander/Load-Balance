1e70718713ae1b45f3c8f5f34e24fcf9d258f585
Nawin chander <nawinnapzy289@gmail.com>
2026-03-28 11:18:18 +0530

    add file

--- Advance/Round Robin.js
+++ Advance/Round Robin.js
@@ -1,2 +1,27 @@
 //// Round Robin Load Balancer
 
+class RoundRobinBalancer {
+  constructor(servers) {
+    this.servers = servers;
+    this.index = 0;
+  }
+
+  getNextServer() {
+    const server = this.servers[this.index];
+    this.index = (this.index + 1) % this.servers.length;
+    return server;
+  }
+}
+
+// Usage
+const balancer = new RoundRobinBalancer([
+  "http://server1",
+  "http://server2",
+  "http://server3",
+]);
+
+console.log(balancer.getNextServer()); // server1
+console.log(balancer.getNextServer()); // server2
+console.log(balancer.getNextServer()); // server3
+
+
