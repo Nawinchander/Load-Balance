f816b703b50e5691161c0b764d32539375478588
Nawin chander <nawinnapzy289@gmail.com>
2026-03-28 11:20:17 +0530

    add code

--- Advance/Consistent Hashing.js
+++ Advance/Consistent Hashing.js
@@ -2,4 +2,49 @@
 
 // Used for caching systems like Redis clusters / CDNs.
 
+const crypto = require("crypto");
+
+class ConsistentHashBalancer {
+  constructor(servers, replicas = 3) {
+    this.ring = new Map();
+    this.sortedKeys = [];
+
+    servers.forEach(server => {
+      for (let i = 0; i < replicas; i++) {
+        const key = this.hash(server + i);
+        this.ring.set(key, server);
+        this.sortedKeys.push(key);
+      }
+    });
+
+    this.sortedKeys.sort();
+  }
+
+  hash(input) {
+    return crypto.createHash("md5").update(input).digest("hex");
+  }
+
+  getServer(key) {
+    const hash = this.hash(key);
+
+    for (let node of this.sortedKeys) {
+      if (hash <= node) {
+        return this.ring.get(node);
+      }
+    }
+
+    return this.ring.get(this.sortedKeys[0]);
+  }
+}
+
+// Usage
+const ch = new ConsistentHashBalancer([
+  "cache1",
+  "cache2",
+  "cache3",
+]);
+
+console.log(ch.getServer("user123"));
+
+
 
